fd0082d258902057b9c1d90796bae1b257f2f592
Roee-R <roi890@gmail.com>
2019-01-18 13:09:23 +0200

    Setup app to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,8 +4,9 @@ mongoose.Promise=global.Promise // tell mongoose that we use promises
 
 // mongoose maintanig the connection over time
 // this method below connect to our local db or from mLab DB
-mongoose.connect('mongodb://Roee_R:roi199115@@ds161024.mlab.com:61024/test-db'
-|| 'mongodb://localhost:27017/TodoApp' ,{ useNewUrlParser: true })
+var mLab = 'mongodb://Roee_R:roi199115@@ds161024.mlab.com:61024/test-db'
+var localHost = 'mongodb://localhost:27017/TodoApp'
+mongoose.connect(process.env.PORT ? mLab : localHost ,{ useNewUrlParser: true })
 
 
 module.exports={mongoose} // same as module.exports.mongoose=mongoose
